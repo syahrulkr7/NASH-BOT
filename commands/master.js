94ea3253bdf3c724af288592f148eeac8c854260
syahrulkr7 <168558459+syahrulkr7@users.noreply.github.com>
2024-05-27 16:55:36 +0700

    Update master.js

--- commands/master.js
+++ commands/master.js
@@ -1,11 +1,11 @@
 module.exports = {
   name: 'mention',
-  description: 'Responds when "josh," "joshua," or "joshua apostol" is mentioned',
+  description: 'Responds when "arul," "pina," or "rul" is mentioned',
   nashPrefix: false,
   execute(api, event, args) {
     const message = event.body.toLowerCase();
-    if (message.includes('josh') || message.includes('joshua') || message.includes('joshua apostol')) {
-      api.sendMessage("Don't call my master, dude", event.threadID, event.messageID);
+    if (message.includes('arul') || message.includes('pina') || message.includes('rul')) {
+      api.sendMessage("Jangan panggil tuanku, kawan", event.threadID, event.messageID);
     }
   },
 };
